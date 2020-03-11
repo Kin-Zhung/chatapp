43a4b08f983bddfe42173f60bbd7cd63b9febeb1
kin-zhung <li-jeremy@outlook.com>
2020-03-10 21:43:57 -0400

    asdf

--- backend/router.js
+++ backend/router.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require(mongoose);
 
 router.get('/', (req,res)=>{
     res.send('server is up and running');
 });
 
+var eventdata =  EventLog.find()
+.exec()
+.then();
+
+var historydata =  Message.find()
+.exec()
+.then();
+
+var historyroom =  Message.find({'chat': 'general'})
+.exec()
+.then();
 module.exports = router;
